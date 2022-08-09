3da4c534da5acf654f19f7b064ed3e45314690db
daaanny90 <danny.spina@hotmail.it>
2022-08-09 15:23:55 +0200

    Add about content

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,22 @@
 <script>
-  import PageTitle from '$lib/components/PageTitle.svelte';
+	import PageTitle from '$lib/components/PageTitle.svelte';
 </script>
 
 <PageTitle title="About Me" subtitle="" />
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, similique? Delectus commodi beatae natus debitis temporibus! Pariatur amet recusandae necessitatibus culpa ipsum dignissimos quasi ad, distinctio architecto suscipit deserunt adipisci!</p>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti sapiente sit id architecto. Beatae inventore earum animi nam quia perferendis necessitatibus reiciendis error. Quam enim deleniti, reprehenderit, amet deserunt assumenda fugiat ab maiores mollitia esse dolor doloremque. Aliquid ab doloremque labore eos ad. Illum saepe corporis culpa sed pariatur quos voluptates cumque veniam iste repellat, nemo quis nesciunt magnam nisi accusantium dolorem deserunt. Quod ducimus quo maxime officia facilis corporis consequuntur, debitis, officiis accusantium minus repudiandae aperiam ipsa quaerat animi possimus itaque rerum voluptatem quibusdam. Quis animi magnam repellendus dolore voluptas laborum veritatis modi molestias, in, autem neque beatae reprehenderit?</p>
+<p>
+	I call myself "a digital craftsman." I am a developer, mostly web, and every day I try to build
+	digital products to the best of my ability. I did not start programming as a child, but at the age
+	of 25.
+</p>
+
+<p>
+	I went through basic training that in the end led me to become, for what it can mean, a
+	"full-stack" developer.
+</p>
+<p>
+	I am passionate about programming and the world of the Internet, open source philosophy and the
+	ethical choices behind software. I work with many different technologies and never get tired of
+	trying new things.
+</p>
+
+<p>See you in the blog.</p>
