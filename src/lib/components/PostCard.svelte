ccfa8bb226ae0d94b5f65e8a9cd71d45fad8713e
Danny Spina <danny@ostec.de>
2023-01-20 13:53:58 +0100

    Add tags in postcard and go back arrow

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -3,15 +3,38 @@
 
 	let dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
 	let pusblishDate = new Date(post.meta.date).toLocaleDateString("gb-GB", dateOptions)
+	console.log(post.meta)
 </script>
 <a href={post.path}>
 	<h2>
 		{post.meta.title}
 	</h2>
 	<h4>{post.meta.subtitle}</h4>
-	<span class="cat">{pusblishDate}</span>
+	<div class="card-footer">
+		<span class="cat">{pusblishDate}</span>
+		<div class="categories">
+			{#each post.meta.categories as category}
+				<span class="cat">{category}</span>
+			{/each}
+		</div>
+	</div>
 </a>
 <style lang="scss" scoped>
+	.card-footer {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: center;
+
+		.categories {
+			.cat {
+				font-size: .7rem;
+			}
+			.cat:not(:first-child) {
+				margin-left: .3rem
+			}
+		}
+	}
   		a {
 			text-decoration: none;
 			color: inherit;
