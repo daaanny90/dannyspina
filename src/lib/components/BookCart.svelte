c0027350907eb33cf3db21246f52dd8e62013e51
Danny Spina <danny@ostec.de>
2023-02-07 11:23:41 +0100

    Change links target to blank

--- src/lib/components/BookCart.svelte
+++ src/lib/components/BookCart.svelte
@@ -2,7 +2,7 @@
   export let link;
 </script>
 
-<a class="affiliate_link" href="{link}"
+<a class="affiliate_link" href="{link}" target="_blank"
       ><svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
