5bb31c4c6199ee5399eef6f59883b8dfbf86c8c2
Danny Spina <danny.spina@hotmail.it>
2023-08-02 15:22:23 +0200

    update reading books

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -40,8 +40,7 @@
 
 <Callout title="I'm reading">
   <strong>Dopamine Nation</strong> by Anna Lembke <br />
-  <strong>The Art of Taking Action</strong> by Gregg Krech <br />
-  <strong>Clean Code</strong> by Robert C. Martin <br />
+  <strong>Dune</strong> by Frank Herbert <br />
 </Callout>
 
 <h2>Categories</h2>
