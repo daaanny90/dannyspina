3665ceb7210b4400cb7db54c2fe0f3eacdf6cc34
Danny Spina <danny.spina@hotmail.it>
2023-08-02 10:15:21 +0200

    add badge

--- src/lib/components/PostGroup.svelte
+++ src/lib/components/PostGroup.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Post } from "$lib/helpers/types";
+  import Badge from "./Badge.svelte";
 
   export let category: string;
   export let posts: Post[];
@@ -17,12 +18,22 @@
     {#each posts.slice(0, 4) as post}
       <li>
         <a href={post.path}>{post.meta.title}</a>
+        {#if post.latest} <Badge text="New" outline/> {/if}
       </li>
     {/each}
   </ul>
 </article>
 
 <style lang="scss" scoped>
+  .badge {
+    background-color: var(--accent-color);
+    color: var(--background-color);
+    padding: .1rem .5rem;
+    margin: 0 .5rem;
+    font-size: .8rem;
+    font-family: "JetBrains Mono", sans-serif;
+  }
+
   article {
     margin-bottom: 3rem;
 
