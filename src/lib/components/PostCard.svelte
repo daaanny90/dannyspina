c5874525cd63ff13f2d4695b1186644b0808796b
Danny Spina <dasp@herole.de>
2025-03-11 12:21:26 +0100

    remove fancy animations

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -58,20 +58,13 @@
     color: inherit;
     height: 100%;
     display: block;
-    border: 0.5px solid var(--text-color);
+    border: 2px solid var(--text-color);
     padding: 1rem;
-    box-shadow: 2px 1px 30px -1px transparent;
-    transition: box-shadow 0.1s linear;
 
     &:visited,
     &:hover {
       text-decoration: none;
       color: inherit;
-    }
-
-    &:hover {
-      box-shadow: 0px 0px 0px 6px var(--text-color);
-      transition: box-shadow 0.1s linear;
     }
   }
 
