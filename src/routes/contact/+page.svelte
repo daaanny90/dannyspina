62e8157b02d2918f1a2350a74d0f320405ba0c1c
Danny Spina <dasp@herole.de>
2025-03-11 12:21:53 +0100

    add some social links to contacts

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,6 +5,13 @@
 <PageTitle title="Get in touch" />
 
 <p>Thank you for visiting my website!</p>
-<p>If you would like to connect with me, I am active on <a href="https://github.com/daaanny90">GitHub</a> – feel free to check out my projects and contributions there.</p>
+  <p>If you would like to connect with me, I am active on different social network and platforms</p>
+<ul>
+  <li><a href="https://github.com/daaanny90">GitHub</a> – feel free to check out my projects and contributions there.</li>
+  <li><a href="https://livellosegreto.it/@danny_sp">Mastodon</a> - I share random stuff here - both in italian and english</li>
+  <li><a href="https://bsky.app/profile/dannyspina.com">Bluesky</a> - same as Mastodon</li>
+  <li><a href="https://www.strava.com/athletes/115600165">Strava</a> - my running activities</li>
+  <li><a href="https://open.spotify.com/show/6DQKst03uhUPBStqSFYVai?si=517c76c4c11d4122">Asincrono</a> - my podcast (in italian)</li>
+</ul>
 <p>Additionally, you can contact me <a href="mailto:hi@danny.com">via email.</a></p>
 <p>I am always open to networking, discussing new projects, or just chatting about technology and development. Don't hesitate to reach out!</p>
